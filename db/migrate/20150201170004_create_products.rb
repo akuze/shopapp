f4edd5bff1348c6336b6e31cad51425f05dae3e1
akuze <akuze@users.noreply.github.com>
2015-02-02 02:28:11 +0800

    Added cart function

---
+++ db/migrate/20150201170004_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :description
+      t.decimal :price, precision: 12, scale: 3
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
