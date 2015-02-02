ef341ee882c547a282244fd6d935a4a04bef6085
akuze <akuze@users.noreply.github.com>
2015-02-02 19:44:48 +0800

    Finished mock check-up

--- app/controllers/check_out_controller.rb
+++ app/controllers/check_out_controller.rb
@@ -2,4 +2,8 @@ class CheckOutController < ApplicationController
   def form
   	@order_items = current_order.order_items
   end
+
+  def complete
+  	@order_items = current_order.order_items
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'check_out/form'
+  get 'check_out/complete'
 
   resources :products, only: [:index]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
-  resources :check_out, only: [:form]
+  resources :check_out, only: [:form, :complete]
 
   devise_for :users
   get 'welcome/index'
